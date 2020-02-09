4f5e5812b2a1b74ac38de75ba7792ab5c2878eb8
AnatDean <anat.dean@hotmail.com>
2020-02-09 14:27:19 +0000

    update restaurant migrations key name

--- db/migrations/20200208104904_create-restaurants-table.js
+++ db/migrations/20200208104904_create-restaurants-table.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
     restaurantsTable.string('rest_name');
     restaurantsTable.boolean('open_late');
     restaurantsTable.boolean('serves_hot_meals');
-    restaurantsTable.integer('rest_area').references('areas.area_id');
+    restaurantsTable.integer('area_id').references('areas.area_id');
     restaurantsTable.string('website');
   });
 };
